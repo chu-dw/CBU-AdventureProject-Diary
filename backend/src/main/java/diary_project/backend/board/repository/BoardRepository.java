5825a7eac8afceadd977c2ba02d73bc4fd5eae3b
DongWon <ehddnjs1747@daum.net>
2023-01-18 22:33:03 +0900

    board 초기

---
+++ backend/src/main/java/diary_project/backend/board/repository/BoardRepository.java
@@ -0,0 +1,6 @@
+package diary_project.backend.board.repository;
+
+import diary_project.backend.user.entity.User;
+
+public interface BoardRepository {
+}
--- backend/src/main/java/diary_project/backend/board/service/BoardService.java
+++ backend/src/main/java/diary_project/backend/board/service/BoardService.java
@@ -1,20 +1,19 @@
-//package diary_project.backend.board.service;
-//
-//
-//import diary_project.backend.board.mapper.BoardMapper;
-//import diary_project.backend.board.dto.BoardDto;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//public class BoardService {
-//
-//    @Qualifier("BoardService")
-//
-//
-//}
+package diary_project.backend.board.service;
+
+
+
+import diary_project.backend.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+
+}
