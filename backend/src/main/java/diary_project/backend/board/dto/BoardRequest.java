b09d270d0e1f3f846dcd766f911378543bca4f56
DongWon <ehddnjs1747@daum.net>
2023-01-25 16:04:00 +0900

    feat: dto

--- backend/src/main/java/diary_project/backend/board/dto/BoardRequest.java
+++ backend/src/main/java/diary_project/backend/board/dto/BoardRequest.java
@@ -1,4 +1,15 @@
 package diary_project.backend.board.dto;
 
+import diary_project.backend.board.entity.Board;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class BoardRequest {
+    private Board board;
 }
--- backend/src/main/java/diary_project/backend/board/dto/BoardResponse.java
+++ backend/src/main/java/diary_project/backend/board/dto/BoardResponse.java
@@ -1,4 +1,14 @@
 package diary_project.backend.board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class BoardResponse {
+    private String maintext;
 }
